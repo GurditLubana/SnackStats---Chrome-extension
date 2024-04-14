018b104e9305d7ab0b765924a8f6d50016b6464f
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-04-14 14:46:37 -0500

    Implemented logic for uberEats

--- scripts/content.js
+++ scripts/content.js
@@ -11,12 +11,63 @@ async function expandAllListItems() {
       await new Promise(resolve => setTimeout(resolve, 2500)); 
       expandListBtn = (orderList.firstChild).lastChild; 
     }
-    console.log("Finished expanding all items.");
+    console.log("\n\nFinished expanding all items.");
+    const pastOrdersList = orderList.firstChild;
+    let orderListJson = {'totalAmountSpent':0, 'mostAmountSpent': {'restaurant':[], 'amountSpent':0}}
+
+    for (var i in pastOrdersList.childNodes){
+      if ((pastOrdersList.childNodes[i]).className === "al"){
+        
+        var orderInfo = (pastOrdersList.childNodes[i]).children[2].children[0].children[0].children[0];
+        var restaurantName = orderInfo.children[0].children[0].innerHTML.trim();
+        var amount = (orderInfo.children[1].children[0].firstChild.textContent).split("$")[1];
+
+        // console.log(restaurantName, amount);
+        amount = amount? parseFloat(amount.trim()): 0.00;
+        
+        if(!(restaurantName in orderListJson)){
+
+          orderListJson[restaurantName] = [1, amount];
+          orderListJson['totalAmountSpent']+= amount;
+
+          if(amount > orderListJson['mostAmountSpent']['amountSpent']){
+            orderListJson['mostAmountSpent']['amountSpent'] = amount;
+            orderListJson['mostAmountSpent']['restaurant'] = [];
+            (orderListJson['mostAmountSpent']['restaurant']).push(restaurantName);
+          }
+          else if(amount === orderListJson['mostAmountSpent']['amountSpent']){
+            (orderListJson['mostAmountSpent']['restaurant']).push(restaurantName);
+          }
+          
+        }
+        else{
+          var numVisited = (orderListJson[restaurantName])[0] + 1;
+          var totalAmount = (orderListJson[restaurantName])[1] + amount;
+          orderListJson[restaurantName] = [numVisited, totalAmount]
+          orderListJson['totalAmountSpent']+= amount;
+
+          if(totalAmount > orderListJson['mostAmountSpent']['amountSpent']){
+            orderListJson['mostAmountSpent']['amountSpent'] = totalAmount;
+            orderListJson['mostAmountSpent']['restaurant'] = [];
+            (orderListJson['mostAmountSpent']['restaurant']).push(restaurantName);
+          }
+          else if(totalAmount === orderListJson['mostAmountSpent']['amountSpent']){
+            (orderListJson['mostAmountSpent']['restaurant']).push(restaurantName);
+          }
+          
+
+
+
+        }
+
+       
+      }
+    }
+    console.log(orderListJson);
   }
   
 
   if (document.readyState === 'complete') {
-    console.log("Hello world")
     expandAllListItems();
   } 
   else {
