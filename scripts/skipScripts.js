0bdaa309afc69f5ccc11583c06f4009932744f8e
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-04-14 19:21:26 -0500

    Implemented expandScreen for skip the dishes

--- scripts/skipScripts.js
+++ scripts/skipScripts.js
@@ -1,10 +1,19 @@
 if (document.readyState === 'complete') {
     console.log("i am ready now");
-    
+    expandScreen();
   } 
   else {
     window.addEventListener('load', ()=>{
         console.log("i am still loading... ");
 
     });
+  }
+
+  function expandScreen(){
+
+    var mainElement = document.querySelector('.styles__Wrapper-sc-1kbgjlb-0');
+    var button = mainElement.querySelector('.MuiButtonBase-root-330');
+    console.log(button);
+    button.click();
+
   }
