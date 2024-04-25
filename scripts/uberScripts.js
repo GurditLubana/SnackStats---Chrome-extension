035db707e9b7d801a656789a9c1671c4412fcca6
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-04-25 14:58:23 -0500

    Implemented monthly Stat function

--- scripts/uberScripts.js
+++ scripts/uberScripts.js
@@ -1,13 +1,8 @@
-
 if (document.readyState === "complete") {
   calculateExpenditure();
 } else {
   window.addEventListener("load", calculateExpenditure);
 }
-
-
-
-
 
 async function calculateExpenditure() {
   const mainContent = document.getElementById("main-content");
@@ -17,12 +12,97 @@ async function calculateExpenditure() {
 
     const orderListJson = {
       totalAmountSpent: 0,
+      totalOrders: 0,
+      months: {
+        Jan: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Feb: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Mar: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Apr: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        May: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Jun: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Jul: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Aug: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Sep: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Oct: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Nov: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+        Dec: {
+          totalAmount: 0,
+          totalOrders: 0,
+          favRest: "",
+          mostOrders: 0,
+          restaurantList: {},
+        },
+      },
       mostAmountSpent: { restaurant: [], amountSpent: 0 },
     };
 
     orderList.childNodes.forEach((node) => {
-      console.log("inside of loop");
-
       if (node.className === "al") {
         processDataNode(node, orderListJson);
       }
@@ -30,9 +110,6 @@ async function calculateExpenditure() {
     console.log(orderListJson);
   }
 }
-
-
-
 
 async function expandOrderList(orderList) {
   let expandListBtn = orderList.lastChild;
@@ -44,27 +121,28 @@ async function expandOrderList(orderList) {
   console.log("Finished expanding all items.");
 }
 
-
-
-
 function processDataNode(node, orderListJson) {
-  const orderInfo = node.children[2].children[0].children[0].children[0];
-  const restaurantName = orderInfo.children[0].children[0].textContent.trim();
-  const amountString = orderInfo.children[1].children[0].firstChild.textContent;
-  let amount = amountString.split("$")[1]
-    ? parseFloat(amountString.split("$")[1].trim())
-    : 0.0;
+  try {
+    const orderInfo = node.children[2].children[0].children[0].children[0];
+    const restaurantName = orderInfo.children[0].children[0].textContent.trim();
+    const amountString =
+      orderInfo.children[1].children[0].firstChild.textContent;
+    const monthString =
+      orderInfo.children[1].children[0].childNodes[2].textContent.trim();
+    let amount = amountString.split("$")[1]
+      ? parseFloat(amountString.split("$")[1].trim())
+      : 0.0;
+    const date = monthString.split("at")[0];
+    const month = date.slice(-7, -4);
+    console.log(restaurantName, amount, month);
 
-  console.log(restaurantName, amount);
-
-  updateOrderStats(restaurantName, amount, orderListJson);
+    updateOrderStats(restaurantName, amount, month, orderListJson);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-
-
-
-
-function updateOrderStats(restaurantName, amount, orderListJson) {
+function updateOrderStats(restaurantName, amount, month, orderListJson) {
   if (!orderListJson[restaurantName]) {
     orderListJson[restaurantName] = { visits: 1, totalSpent: amount };
   } else {
@@ -72,13 +150,33 @@ function updateOrderStats(restaurantName, amount, orderListJson) {
     orderListJson[restaurantName].totalSpent += amount;
   }
 
+  updateMonthlyStats(restaurantName, amount, month, orderListJson);
+
   orderListJson.totalAmountSpent += amount;
 
   updateMostSpent(restaurantName, amount, orderListJson);
 }
 
+function updateMonthlyStats(restaurantName, amount, month, orderListJson) {
+  
+  orderListJson["months"][month]["totalAmount"] += amount;
+  orderListJson["months"][month]["totalOrders"] += 1;
 
+  if (!orderListJson["months"][month]["restaurantList"][restaurantName]) {
+    orderListJson["months"][month]["restaurantList"][restaurantName] = 1;
+  } else {
+    orderListJson["months"][month]["restaurantList"][restaurantName] += 1;
+  }
 
+  if (
+    orderListJson["months"][month]["mostOrders"] <
+    orderListJson["months"][month]["restaurantList"][restaurantName]
+  ) {
+    orderListJson["months"][month]["mostOrders"] =
+      orderListJson["months"][month]["restaurantList"][restaurantName];
+    orderListJson["months"][month]["favRest"] = restaurantName;
+  }
+}
 
 function updateMostSpent(restaurantName, amount, orderListJson) {
   const mostSpent = orderListJson.mostAmountSpent;
@@ -94,5 +192,3 @@ function updateMostSpent(restaurantName, amount, orderListJson) {
     mostSpent.restaurant.push(restaurantName);
   }
 }
-
-
